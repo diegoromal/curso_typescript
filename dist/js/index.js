81402836d48882d9775168d2b009afdd104c7fa3
Diego RA <diego@qosit.com.br>
2023-08-11 10:38:36 -0300

    Initial commit

---
+++ dist/js/index.js
@@ -0,0 +1,18 @@
+"use strict";
+let x = 10;
+x = 20;
+let firstName = "Diego";
+let age = 31;
+const isAdmin = true;
+// object
+const myNumbers = [1, 2, 3];
+myNumbers.push(5);
+// tuplas
+let myTuple;
+// object literals
+const user = { name: "João", age: 33 };
+// any
+let a = 0;
+a = "teste";
+a = true;
+console.log(x);
