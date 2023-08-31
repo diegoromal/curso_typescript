a63f4955e5d32719d9a89f6685a2d3bb651d6aed
Diego RA <diego@qosit.com.br>
2023-08-31 18:08:25 -0300

    React Project - Task List

--- 12_PROJETO_REACT/todo_react/src/App.tsx
+++ 12_PROJETO_REACT/todo_react/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 // components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -12,17 +12,22 @@ import styles from "./App.module.css";
 import { ITask } from "./interfaces/Task";
 
 function App() {
+    const [taskList, setTaskList] = useState<ITask[]>([]);
     return (
         <div>
             <Header />
             <main className={styles.main}>
                 <div>
                     <h2>O que você vai fazer</h2>
-                    <TaskForm btnText="Criar Tarefa" />
+                    <TaskForm
+                        btnText="Criar Tarefa"
+                        taskList={taskList}
+                        setTaskList={setTaskList}
+                    />
                 </div>
                 <div>
                     <h2>Suas tarefas:</h2>
-                    <TaskList />
+                    <TaskList taskList={taskList} />
                 </div>
             </main>
             <Footer />
--- 12_PROJETO_REACT/todo_react/src/components/TaskList.tsx
+++ 12_PROJETO_REACT/todo_react/src/components/TaskList.tsx
@@ -1,12 +1,31 @@
 import React from "react";
 
-interface Props {}
+// interfaces
+import { ITask } from "../interfaces/Task";
 
-const TaskList = (props: Props) => {
+// CSS
+import styles from "./TaskForm.module.css";
+
+interface Props {
+    taskList: ITask[];
+}
+
+const TaskList = ({ taskList }: Props) => {
     return (
-        <div>
-            <p>Lista de tarefa</p>
-        </div>
+        <>
+            {taskList.length > 0 ? (
+                taskList.map((task) => (
+                    <div key={task.id}>
+                        <div>
+                            <h4>{task.title}</h4>
+                            <p>Dificuldade: {task.difficulty}</p>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <p>Não há tarefas cadastradas</p>
+            )}
+        </>
     );
 };
 
