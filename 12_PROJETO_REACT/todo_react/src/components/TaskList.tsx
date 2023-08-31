20bd4eeafc84f5c5c043f59c03e573563e2cfd8a
Diego RA <diego@qosit.com.br>
2023-08-31 20:55:11 -0300

    React Project - Task List - Styles

--- 12_PROJETO_REACT/todo_react/src/components/TaskList.tsx
+++ 12_PROJETO_REACT/todo_react/src/components/TaskList.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import { ITask } from "../interfaces/Task";
 
 // CSS
-import styles from "./TaskForm.module.css";
+import styles from "./TaskList.module.css";
 
 interface Props {
     taskList: ITask[];
@@ -15,10 +15,14 @@ const TaskList = ({ taskList }: Props) => {
         <>
             {taskList.length > 0 ? (
                 taskList.map((task) => (
-                    <div key={task.id}>
-                        <div>
+                    <div key={task.id} className={styles.task}>
+                        <div className={styles.details}>
                             <h4>{task.title}</h4>
                             <p>Dificuldade: {task.difficulty}</p>
+                        </div>
+                        <div className={styles.actions}>
+                            <i className="bi bi-pencil"></i>
+                            <i className="bi bi-trash"></i>
                         </div>
                     </div>
                 ))
