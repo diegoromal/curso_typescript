b52ff84a049f047cda16f40b31f3dd6fac60cce8
Diego RA <diego@qosit.com.br>
2023-09-03 21:34:23 -0300

    Created swagger docs

--- 14_TS_API_RESTFUL/src/app.ts
+++ 14_TS_API_RESTFUL/src/app.ts
@@ -2,12 +2,17 @@
 require("dotenv").config();
 
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 import config from "config";
 
 const app = express();
 
 // JSON middleware
 app.use(express.json());
+
+// Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // DB
 import db from "../config/db";
